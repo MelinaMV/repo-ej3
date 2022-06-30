10a69e5df98ab409b506715f266e43f403fdcd6f
Melina Martinez Vega <melimv97@gmail.com>
2022-06-30 17:46:07 -0300

    agrego todo

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let nombre
+
+nombre = prompt ('Ingrese su nombre')
+
+console.log(nombre)
+
+let edad = prompt('Ingrese su edad')
+
+if(edad < 30){
+    console.log('Hola, ¿como andas?')
+} else {
+    console.log('Bienvenido. Buenos días')
+}
+
+
+
